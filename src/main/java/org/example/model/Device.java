24b3752194afe39c7b1b465a27e3c5cb1375e172
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-05-18 12:32:57 +0300

    second work

--- src/main/java/org/example/model/Device.java
+++ src/main/java/org/example/model/Device.java
@@ -7,12 +7,12 @@ import lombok.Data;
 @Table(name = "devices")
 @Data
 public class Device {
-    @Id
-//    @Column(name = "user_id")
-    private Integer userId;
+
+
+    @ManyToOne
+    private User user;
     private String deviceName;
+    @Id
     @Column(unique = true)
     private int mac;
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private User userOne;
-}
+    }
--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -18,6 +18,6 @@ public class User {
     private String lastName;
     private String phoneNumber;
     private String gender;
-    @OneToMany (mappedBy = "userOne", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany (mappedBy = "user")
     private List<Device> devices;
 }
